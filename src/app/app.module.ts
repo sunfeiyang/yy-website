a80b8e00fcce7031c1f3759711caf03d7c7f011b
sunfeiyang <sfy5941@163.com>
2018-12-19 17:59:21 +0800

    one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CulturePoemComponent } from './culture-poem/culture-poem.component';
+import { CultureAllegoricalComponent } from './culture-allegorical/culture-allegorical.component';
+import { CultureDicComponent } from './culture-dic/culture-dic.component';
+import { CultureFamousComponent } from './culture-famous/culture-famous.component';
+import { CultureIdiomComponent } from './culture-idiom/culture-idiom.component';
+import { CultureTodayhistoryComponent } from './culture-todayhistory/culture-todayhistory.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    CulturePoemComponent,
+    CultureAllegoricalComponent,
+    CultureDicComponent,
+    CultureFamousComponent,
+    CultureIdiomComponent,
+    CultureTodayhistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
